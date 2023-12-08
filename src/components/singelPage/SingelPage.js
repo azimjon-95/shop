a3d2f370071cbe4d08e105769a1c2b68c58dbe19
vorisxon <vorisxon.me@gmail.com>
2023-12-08 17:21:50 +0500

    hello gruppa

--- src/components/singelPage/SingelPage.js
+++ src/components/singelPage/SingelPage.js
@@ -1,5 +1,5 @@
-import React from 'react';
-// import './';
+import React, { useState } from 'react';
+import "./singlepage.css"
 import { useParams } from 'react-router-dom';
 import Data from '../../Data'
 
@@ -7,19 +7,29 @@ import Data from '../../Data'
 
 const SingelPage = () => {
 
-
     const { id } = useParams()
 
-
-    const result = Data.filter(i => i.id === id)
+  const result = Data.find(item => item.id === parseInt(id, 10));
 
     console.log(result);
 
+   
+
     return (
-        <div>
-
-
-        </div>
+        <div className='Card'>
+      {result ? (
+        <>  
+          <img src={result.images[0]} alt="" />
+          <h1>{result.title}</h1>
+          <p>narxi : {result.price}</p>
+          <p>{result.description}</p>
+          <p>sizes : {result.sizes}</p>
+          { }
+        </>
+      ) : (
+        <div>Элемент не найден</div>
+      )}
+    </div>
     )
 }
 
